8cd5bb79face3ec75e8f9629165e245d9296bed1
ELSHODBEK <elshodxushvaqtov07@gmail.com>
2023-04-03 18:57:43 +0500

    fragment-setTheme

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat.ThemeCompat
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_MyApplication)
         setContentView(binding.root)
-//        supportFragmentManager.beginTransaction()
-//            .add(R.id.container,RegistrationFragment())
-//            .commit()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container,RegistrationFragment())
+            .commit()
     }
 }
