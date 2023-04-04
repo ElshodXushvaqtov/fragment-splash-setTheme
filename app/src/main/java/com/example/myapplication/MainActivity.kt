6611426296c78977b1667c5e92b6b222ddd95276
PC680 <elshodxushvaqtov07@gmail.com>
2023-04-04 10:27:28 +0500

    noactionbar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.core.content.res.ResourcesCompat.ThemeCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding=ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.Theme_MyApplication)
         setContentView(binding.root)
-
         supportFragmentManager.beginTransaction()
             .add(R.id.container,RegistrationFragment())
             .commit()
